bee5a164897272c79e1d06961daf4603ed042652
AldairMC <aldairmc2023@gmail.com>
2020-04-07 23:35:26 -0500

    :sparkles: Points 2-6, 8 ready

--- covid_nineteen.py
+++ covid_nineteen.py
@@ -1,4 +1,4 @@
-# import numpy as np
+import numpy as np
 import pandas as pd
 # import matplotlib.pyplot as plt
 
@@ -28,11 +28,63 @@ CONTRY = 'País de procedencia'
 URL = 'casos.csv'
 DATA = pd.read_csv(URL)
 
+def MUNICIPALITIES():
+    return(pd.Series([i.upper() for i in DATA[CITY]]))
+
+def DEPARTMENTS():
+    return(pd.Series([i.upper() for i in DATA[DEPARTMENT]]))
+
+def FILTER(key, keyword):
+    return(DATA[DATA[key] == keyword][key])
+
 # 1. Número de casos de Contagiados en el País.
 def total_infected():
-    return(len(DATA[ID]))
+    return(DATA[ID].size)
 
+# 2. Número de Municipios Afectados
+def num_affected_municipalities():
+    return(MUNICIPALITIES().unique().size)
+
+# 3. Liste los municipios afectados (sin repetirlos)
+def list_affected_municipalities():
+    return(MUNICIPALITIES().unique())
+
+# 4. Número de personas que se encuentran en atención en casa
+def atention_in_home():
+    return(FILTER(ATENTION, 'Casa').size)
+
+#5. Número de personas que se encuentran recuperados
+def num_people_recovered():
+    return(FILTER(ATENTION, 'Recuperado').size)
+
+#6. Número de personas que ha fallecido
+def num_people_killed():
+    return(FILTER(ATENTION, 'Fallecido').size)
+
+#7. Ordenar de Mayor a menor por tipo de caso (Importado, en estudio, Relacionado)
+# INCOMPLET
+# def order_type_of_case():
+#     importado = FILTER(TYPE, 'Importado').size
+#     study = FILTER(TYPE, 'En estudio').size
+#     relacionado = FILTER(TYPE, 'Relacionado').size
+#     types = ['Importado', 'En estudio', 'Relacionado']
+#     arr = pd.Series([importado, study, relacionado])
+#     arr.set_index(types, inplace=True)
+#     print(arr.sort_values(ascending=False))
+    # print(importado, study, relacionado)
+
+#8. Número de departamentos afectados
+def num_affected_department():
+    return(DEPARTMENTS().unique().size)
+
+
+# Function testing
 def result():
-    print("The total infected is: ", total_infected())
-
+    print("1. The total infected is: ", total_infected())
+    print("2. The numbers of the affected municipalities is: ", num_affected_municipalities())
+    print("3. The affected municipalities are: ", list_affected_municipalities())
+    print("4. Those cared for at home are: ", atention_in_home())
+    print("5. Number of people recovered is: ", num_people_recovered())
+    print("6. Number of dead people is: ", num_people_killed())
+    print("8. The numbers of the affected departments is: ", num_affected_department())
 result()
